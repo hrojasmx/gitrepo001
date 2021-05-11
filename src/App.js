377cdf036eb096bd6715fea70380843510966fae
hector <hrpxa270@hotmail.com>
2021-05-11 07:48:01 -0400

    some change to app.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return <div className="App">learn react</div>;
+  return (
+    <div className="App">
+      learn react
+    </div>
+  )
 }
 
 export default App;
